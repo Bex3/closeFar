045f8cd3f49c731598a6c9b3cf0a85a730b86784
bex3 <Rebeccam.bearden@gmail>
2016-09-21 17:09:23 -0400

    deleted a class

--- src/day33CloseFarRunner.java
+++
@@ -1,8 +0,0 @@
-/**
- * Created by bearden-tellez on 9/21/16.
- */
-public class day33CloseFarRunner {
-    public static void main(String[] args) {
-        CloseFarSolver myCloseFarSolver = new CloseFarSolver();
-    }
-}
